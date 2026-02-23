bf53d346722e419d0622557b62e5e426f9a8059f
xokimovshamsiddin17-creator <xokimovshamsiddin17@gmail.com>
2026-02-23 19:22:37 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot ishlayapti 🚀")
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
